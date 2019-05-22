52b4767f486eb1b5476dd6c1b1b91014ebdd42d5
tmaia <taiane.maia@fecap.br>
2019-05-22 16:07:52 -0300

    Alteração do componente Oferta para trazer as ofertas dinamicamente

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Oferta } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
-  selector: 'app-oferta',
-  templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+	selector: 'app-oferta',
+	templateUrl: './oferta.component.html',
+	styleUrls: ['./oferta.component.css'],
+	providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+	public oferta: Oferta;
 
-  ngOnInit() {
-  }
+	constructor(
+		private route: ActivatedRoute, 
+		private ofertasService: OfertasService )
+	{ }
+
+	ngOnInit() {
+		this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
+			.then((resposta: Oferta) => this.oferta = resposta)
+			.catch(error => console.log(error));
+	}
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,7 +2,7 @@ import { Oferta } from './shared/oferta.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-//import 'rxjs/add/operator/toPromise';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
@@ -10,14 +10,20 @@ export class OfertasService {
     constructor(private http: HttpClient) { }
     
     public getOfertas() : Promise<Oferta[]> {
-        return this.http.get('http://localhost:3000/ofertas')
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta );
     }
 
     public getOfertasPorCategoria(categoria: string) : Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta);
+    }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${URL_API}/${id}`)
+            .toPromise()
+            .then((resposta: Oferta) => resposta);
     }
 }
